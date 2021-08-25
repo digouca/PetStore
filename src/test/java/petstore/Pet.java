41e0ed5299e87e1967ff60c1c3c16e83c360e349
digouca <51510287+digouca@users.noreply.github.com>
2021-08-24 22:04:16 -0300

    Completei o script excluirPet (GET)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -68,7 +68,44 @@ public class Pet {
                 .extract()
                         .path("category.name")
         ;
-        System.out.println("O token é " + token);
+        System.out.println("O token e " + token);
+    }
+    @Test(priority=3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Laika"))
+                .body("status", is("sold"))
+
+        ;
+    }
+
+    @Test(priority=4)
+    public void excluirPet(){
+        String petId = "19810205";
+
+       given()
+               .contentType("application/json")
+               .log().all()
+       .when()
+               .delete(uri + "/" + petId)
+       .then()
+               .log().all()
+               .statusCode(200)
+               .body("code", is(200))
+               .body("type", is("unknown"))
+               .body("message", is("19810205"))
+
+       ;
     }
 
 }
